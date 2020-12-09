fe4b6e49373091a526d86c5add770989fd5bf32f
Brian <godonanabt@gmail.com>
2020-12-09 14:59:25 -0600

    Documented Code, Removed Features
    Documented Code fully
    
    Removed features related to accessing data for all 50 states at once
    
    Removed features related to access historical data

--- src/main/java/org/cs3380project/application/backend/CovidUnitedStatesAPI.java
+++ src/main/java/org/cs3380project/application/backend/CovidUnitedStatesAPI.java
@@ -1,21 +1,11 @@
 package org.cs3380project.application.backend;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.util.Scanner;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 /**
@@ -25,80 +15,82 @@ import org.json.JSONObject;
  */
 public class  CovidUnitedStatesAPI {
 
-    private static final String uriBase = "https://api.covidtracking.com";
+    private static final String urlBase = "https://api.covidtracking.com";
 
-    private static final String statesMetadata = "/v1/states/info.json";
     private static final String specificStateMetadata = "/v1/states/%s/info.json";
-
-    private static final String currentValuesAllStates = "/v1/states/current.json";
-    private static final String historicValuesAllStates = "/v1/states/daily.json";
-
     private static final String currentValuesSingleState = "/v1/states/%s/current.json";
     private static final String historicValuesSingleState = "/v1/states/%s/daily.json";
 
-    private static JSONObject getJsonObject(String target) {
-        return getJSONObjectHelper(uriBase + target);
-    }
 
-    private static JSONObject getJsonObject(String target, String state) {
-        return getJSONObjectHelper(uriBase + String.format(target, state.toLowerCase()));
-    }
-
-    private static JSONObject getJSONObjectHelper(String uriComplete) {
-        System.out.printf("Request URI %s\n", uriComplete);
-
+    /**
+     * Does the heavy lifting of creating the API request, retrieving and interpreting response, and returning data
+     * @param target String path to append
+     * @param state State to format URL path with
+     * @return JSONObject with data, null otherwise
+     */
+    private static JSONObject getJSONObjectHelper(String target, String state) {
+        // Builds entire URL from URL base and formatted URL path
+        String urlComplete = urlBase + String.format(target, state.toLowerCase());
         try {
-            int responseCode = -999;
-            int counter = -1;
-            URL url = new URL(uriComplete);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int responseCode;
+            int counter = 0;
+            URL url = new URL(urlComplete);
+            HttpURLConnection conn = null;
+
+            // Makes, at a maximum, three response requests before giving up
             do {
+                conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
                 conn.connect();
                 responseCode = conn.getResponseCode();
-                System.out.printf("Response Code: %d\n", responseCode);
-
                 counter ++;
             } while(responseCode != 200 && counter < 3);
 
+            // If response code is not 'OK'
             if (responseCode != 200)
                 return null;
 
 
-
-
-
-            System.out.println("Reading lines");
             String jsonResponse = null;
 
+            // Open input stream to URL
             InputStream stream = url.openStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-            try{
-                StringBuilder response = new StringBuilder();
-                String responseLine;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
+                try {
+                    // Read through entire downloadable response and store in a string
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    jsonResponse = response.toString();
 
-                jsonResponse = response.toString();
-            } catch(Exception e) {
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    // Shut down all streams when everything is done/crashed
+                    br.close();
+                    stream.close();
+                }
+            } catch(Exception e){
                 System.out.println(e.getMessage());
-            } finally {
-                br.close();
-                stream.close();
+                return null;
             }
 
+            // If no jsonResponse, return null
             if (jsonResponse == null)
                 return null;
 
-            if (jsonResponse.charAt(0) != '{'){
-                System.out.println("Some trimming needed.");
-                jsonResponse = jsonResponse.substring(1, jsonResponse.length() - 1);
+            JSONObject returnJSON = null;
+            try {
+                // Attempts to build a JSON object from the JSON response
+                returnJSON = new JSONObject(jsonResponse);
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+                return null;
             }
 
-            System.out.println("Done, returning");
-
-            return new JSONObject(jsonResponse);
+            return returnJSON;
         } catch (Exception e) {
             // Display error message.
             System.out.println(e.getMessage());
@@ -108,31 +100,21 @@ public class  CovidUnitedStatesAPI {
     }
 
 
-    public static JSONObject getStatesMetadata(){
-        return getJsonObject(statesMetadata);
-    }
-
+    /**
+     * Returns information about how and where the database retrieves information from a specific state
+     * @param state State to retrieve information about
+     * @return JSONObject with data, null otherwise
+     */
     public static JSONObject specificStateMetadata(String state){
-        return getJsonObject(specificStateMetadata, state);
+        return getJSONObjectHelper(specificStateMetadata, state);
     }
 
+    /**
+     * Returns the most recent COVID-19 data for a specific state
+     * @param state State to retrieve data about
+     * @return JSONObject with data, null otherwise
+     */
     public static JSONObject currentValuesSingleState(String state){
-        return getJsonObject(currentValuesSingleState, state);
+        return getJSONObjectHelper(currentValuesSingleState, state);
     }
-
-    public static JSONObject historicValuesSingleState(String state){
-        return getJsonObject(historicValuesSingleState, state);
-    }
-
-    public static JSONObject currentValuesAllStates(){
-        return getJsonObject(currentValuesAllStates);
-    }
-
-    public static JSONObject historicValuesAllStates(){
-        return getJsonObject(historicValuesAllStates);
-    }
-
-
-
-
 }
